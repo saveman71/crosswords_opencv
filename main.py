5c33d4ef809ffb2696f537c19c1b592fb8831608
Antoine Bolvy <antoine.bolvy@gmail.com>
2018-10-31 21:32:57 +0100

    cleanup: pep8 autoformat

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ img = cv2.imread(sys.argv[1], 0)
 cells_w = 14
 cells_h = 18
 
-img = cv2.resize(img, (math.floor(img.shape[1] * 2), math.floor(img.shape[0] * 2)), cv2.INTER_CUBIC);
+img = cv2.resize(img, (math.floor(
+    img.shape[1] * 2), math.floor(img.shape[0] * 2)), cv2.INTER_CUBIC)
 
-plt.imshow(img, cmap='gray'),plt.show()
+plt.imshow(img, cmap='gray'), plt.show()
 
 cell_w = img.shape[1] / cells_w
 cell_h = img.shape[0] / cells_h
 
-output = [[{'type': 'unknown'} for x in range(cells_w)] for y in range(cells_h)]
+output = [[{'type': 'unknown'}
+           for x in range(cells_w)] for y in range(cells_h)]
 
 size = math.floor(cell_w * 0.70)
 
@@ -35,7 +37,8 @@ for cell_y in range(0, cells_h):
         cell = cell[1:-1, 1:-1]
 
         if np.sum(cell < (255//2))/(cell.shape[0]*cell.shape[1]) > 0.5:
-            output[cell_y][cell_x] = {'type': 'blank', 'x': cell_x, 'y': cell_y};
+            output[cell_y][cell_x] = {
+                'type': 'blank', 'x': cell_x, 'y': cell_y}
         else:
             height = cell.shape[0]
             width = cell.shape[1]
@@ -44,23 +47,25 @@ for cell_y in range(0, cells_h):
             cell = cell[pad_h:size+pad_h, pad_w:size+pad_w]
 
             # print(cell.shape)
-            output[cell_y][cell_x] = {'type': 'text', 'data': cell, 'x': cell_x, 'y': cell_y};
+            output[cell_y][cell_x] = {'type': 'text',
+                                      'data': cell, 'x': cell_x, 'y': cell_y}
 
-            ret,thresh = cv2.threshold(cell,180,255,0)
+            ret, thresh = cv2.threshold(cell, 180, 255, 0)
             thresh = cv2.bitwise_not(thresh)
             # plt.imshow(thresh, cmap='gray'),plt.show()
-            im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            im2, contours, hierarchy = cv2.findContours(
+                thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnt = contours[0]
-            x,y,w,h = cv2.boundingRect(cnt)
+            x, y, w, h = cv2.boundingRect(cnt)
             # cv2.rectangle(cell,(x,y),(x+w,y+h),(0,255,0),2)
-            output[cell_y][cell_x]['cropped'] = cell[y-1:y+h+2,x-1:x+w+2]
+            output[cell_y][cell_x]['cropped'] = cell[y-1:y+h+2, x-1:x+w+2]
             # cv2.drawContours(cell, contours, -1, (0,255,0), 3)
             # plt.imshow(cell, cmap='gray'),plt.show()
             # plt.imshow(output[cell_y][cell_x]['cropped'], cmap='gray'),plt.show()
 
-lines = [];
+lines = []
 
-current = [];
+current = []
 # Find all the columns of text
 for cell_x in range(0, cells_w):
     for cell_y in range(0, cells_h):
@@ -105,14 +110,15 @@ for line in lines:
     # create a new array with a size large enough to contain all the images
     final_image = np.ones((sequence_h, sequence_w), dtype=np.uint8) * 255
 
-    current_x = 5 # keep track of where your current image was last placed in the x coordinate
+    current_x = 5  # keep track of where your current image was last placed in the x coordinate
     for cell in sequence:
         # add an image to the final array and increment the x coordinate
 
         # center vertically our image
         center_pad = (sequence_h-cell.shape[0]) // 2
 
-        final_image[center_pad:center_pad+cell.shape[0],current_x:current_x+cell.shape[1]] = cell
+        final_image[center_pad:center_pad+cell.shape[0],
+                    current_x:current_x+cell.shape[1]] = cell
         current_x += cell.shape[1] + 5
 
     # plt.imshow(final_image, cmap='gray'),plt.show()
@@ -154,7 +160,8 @@ for line in output:
             print('-', end='')
         elif 'letter' in cell:
             print(cell['letter'], end='')
-            plt.annotate(cell['letter'], xy=(cell['x'] * cell_w, cell['y'] * cell_h))
+            plt.annotate(cell['letter'], xy=(
+                cell['x'] * cell_w, cell['y'] * cell_h))
         else:
             print('?', end='')
     print()
